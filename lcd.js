2d40a9da6057bc80c06642b833b20589c33a07d2
Elena Seredova <e.grishch@gmail.com>
2017-01-18 18:34:57 +0300

    add url & depth

--- lcd.js
+++ lcd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-var argv = require('yargs')
+let argv = require('yargs')
     .usage('Usage: $0 lcd [--x=depth] [--url=url]')
     .command('depth','specify MAX depth of DOM here')
     .command('url', 'specify the desired url')
@@ -10,7 +10,9 @@ var argv = require('yargs')
     .alias('h', 'help')
     .argv;
 
-// console.log('(%d,%d)', argv.depth, argv.url);
+let url = argv.url;
+let depth = argv.depth;
 
+module.exports = url, depth;
 // The rest of the code will be here... Maybe.
 
