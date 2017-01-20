58a3c294c3dff8a1d74edfbd030f14fb4b9dda90
Elena Seredova <e.grishch@gmail.com>
2017-01-20 17:02:00 +0300

    use html-dom-parser and rename functions

--- index3.js
+++ index3.js
@@ -1,23 +1,23 @@
 'use strict';
 
 const request = require('sync-request');
-const parse5 = require('parse5');
+const dom = require('html-dom-parser');
 const depth = process.argv[2];
 const url = process.argv[3];
 
-var response;
-
-function getHTML(url) {
-    response = request('GET', url);
-    return response.getBody('utf8');
-}
-function parseHTML(response) {
-    const document = parse5.parse(response.getBody('utf8'));
-    console.log(document.childNodes);
+function getDOM(url) {
+    const response = request('GET', url);
+    // console.log(dom(response.getBody('utf8')));
+    return dom(response.getBody('utf8'));
 }
 
+function getHTML(arr) {
+    // console.log(arr[1]);
+    return arr[1];
+}
 
 
-getHTML(url);
-parseHTML(response);
+
+
+
 
