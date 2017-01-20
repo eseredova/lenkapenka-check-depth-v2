0537da3c474b3f4cb15e17c8951c01d3e041b88a
Elena Seredova <e.grishch@gmail.com>
2017-01-20 17:44:16 +0300

    rename to args and use yargs properly

--- args.js
+++ args.js
@@ -1,7 +1,6 @@
-#!/usr/bin/env node
 'use strict';
 
-let argv = require('yargs')
+const argv = require('yargs')
     .usage('Usage: $0 lcd [--x=depth] [--url=url]')
     .command('depth','specify MAX depth of DOM here')
     .command('url', 'specify the desired url')
@@ -10,9 +9,11 @@ let argv = require('yargs')
     .alias('h', 'help')
     .argv;
 
-let url = argv.url;
-let depth = argv.depth;
+const depth = argv.depth;
+const url = argv.url;
 
-module.exports = url, depth;
-// The rest of the code will be here... Maybe.
+module.exports = {
+    depth,
+    url
+};
 
