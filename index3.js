617958e421ecfe04d6aac1da498ac74c3e0c5f16
Elena Seredova <e.grishch@gmail.com>
2017-01-20 17:14:14 +0300

    remove unnecessary console logs

--- index3.js
+++ index3.js
@@ -7,12 +7,10 @@ const url = process.argv[3];
 
 function getDOM(url) {
     const response = request('GET', url);
-    // console.log(dom(response.getBody('utf8')));
     return dom(response.getBody('utf8'));
 }
 
 function getHTML(arr) {
-    // console.log(arr[1]);
     return arr[1];
 }
 
